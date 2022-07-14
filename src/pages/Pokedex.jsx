754936f38b2541502a70c0ff98c3964f8b268e96
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-07-14 14:39:20 -0300

    Pokedex Project

---
+++ src/pages/Pokedex.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import PokemonCard from '../components/PokemonCard';
+
+const Pokedex = () => {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    'https://pokeapi.co/api/v2/pokemon?limit=10',
+  );
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    setLoadMore(data.next);
+
+    function createPokemonObject(results) {
+      results.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,
+        );
+        const data = await res.json();
+        setAllPokemons((currentList) => [...currentList, data]);
+        await allPokemons.sort((a, b) => a.id - b.id);
+      });
+    }
+    createPokemonObject(data.results);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  return (
+    <div className='app-container'>
+      <h1 className='pokedex-title'>Pokedex</h1>
+      <div className='pokemon-container'>
+        <div className='all-container'>
+          {allPokemons.map((pokemonStats, index) => (
+            <PokemonCard
+              key={index}
+              id={pokemonStats.id}
+              image={pokemonStats.sprites.other.dream_world.front_default}
+              name={pokemonStats.name}
+              type={pokemonStats.types[0].type.name}
+            />
+          ))}
+        </div>
+        <button className='load-more' onClick={() => getAllPokemons()}>
+          LOAD MORE
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Pokedex;
